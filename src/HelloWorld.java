64cca82881b23eed77eeeba9836e0ed9b3ce1af5
Michal Mrozek <m.mrozek@hotmail.com>
2022-08-07 15:21:01 +0100

    zmienna

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("helo world");
+        String name = "hello world";
+        System.out.println(name);
 
     }
 }
